6373f7a5e481f918b5e381f2b07552bd2d9e94ad
Fairuz H A <127818365+iusantuy@users.noreply.github.com>
2023-09-27 20:48:00 +0700

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -2,13 +2,21 @@ const mongodb = require('mongodb').MongoClient;
 const config = require('../configs');
 const options = require('./options');
 
+const client = new MongoClient(config.uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+})
+
 const db = new mongodb(config.uri, options);
 
 (async(dbName) => {
     try { 
        await db.connect()
         .then(c => { 
-            return c.db(dbName)
+            return c.db(dbName).command({ping: 1]);
         });
         console.log('Connected to database')
      } 
